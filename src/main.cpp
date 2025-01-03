5edef7529fc754dc92f5063a46211cc58e01180b
Jacob Morales <jacobmorales2001@outlook.com>
2025-01-03 00:21:53 -0800

    proj: reshuffle metal dep structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+
 int main(int argc, char **argv)
 {
     std::cout << "Hello world!" << std::endl;
